3be5682a8129a8eff4fdeccbe8ebbc1bed116f54
Orion1951 <84632050+Orion1951@users.noreply.github.com>
2021-08-05 22:35:14 -0500

    clase para errores

---
+++ WebApi/Errors/CodeErrorResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace WebApi.Errors
+{ 
+public class CodeErrorResponse
+{
+      public CodeErrorResponse(int statusCode, string message = null)
+      {
+         StatusCode = statusCode;
+         Message = message ?? GetDefaultMessageStatusCode(statusCode);
+      }
+
+      private string GetDefaultMessageStatusCode(int statusCode)
+      {
+         return statusCode switch
+         {
+            400 => "El request enviado tiene errores",
+            401 => "Sin autorizacion para este recurso",
+            404 => "Recurso no encontrado",
+            500 => "Se produjo un error de servidor", => null
+         };
+      }
+      public int StatusCode { get; set; }
+      public string Message { get; set; }
+   }
+}
